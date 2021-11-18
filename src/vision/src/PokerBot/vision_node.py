456988af1b51111072d5704d315f3633aec73740
Class Account <ee106a-afr@c105-8.local>
2021-11-17 16:29:04 -0800

    updated vision node

--- src/vision/src/PokerBot/vision_node.py
+++ src/vision/src/PokerBot/vision_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from card_detector import classify
+from darknet import *
 
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -11,12 +11,14 @@ class VisionPublisher:
     #Callback for when an image is received
     def imgReceived(self, img):
         #Save the image in the instance variable
-        self.lastImage = img
-        im = self.bridge.imgmsg_to_cv2(img, desired_encoding="rgb8")
-        print(im.shape)
-        classification = classify(im, 0.01)
+        # self.lastImage = img
+        im = self.bridge.imgmsg_to_cv2(img)
+        r = detect_np(self.net, self.meta, im, 0.1)
+        # classification = classify(im)
         #Print an alert to the console
-        print(str(classification))
+        print("received", rospy.get_time())
+        print(str(r))
+        self.pub.publish(str(r))
 
     #When another node calls the service, return the last image
     def getLastImage(self, request):
@@ -31,15 +33,19 @@ class VisionPublisher:
         self.lastImage = None
         self.bridge = CvBridge()
 
+        # Import neural net metadata
+        self.net = load_net(b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/tiny2-coco.cfg", b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/tiny2_coco_100k_candidate.weights", 0)
+        self.meta = load_meta(b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/output.data")
+
         #Initialize the node
         rospy.init_node('cam_listener')
 
         #Subscribe to the image topic
-        rospy.Subscriber("/cameras/left_hand_camera/image", Image, self.imgReceived)
+        rospy.Subscriber("/usb_cam/image_raw", Image, self.imgReceived)
 
         #Create the publisher
-        self.pub = rospy.Publisher('testtestestestestsetsetsetes', String, queue_size=10)
-        self.r = rospy.Rate(10) # 10 hz 
+        self.pub = rospy.Publisher('/pokerbot/card', String, queue_size=10)
+        self.r = rospy.Rate(100) # 10 hz 
 
     def run(self):
         rospy.spin()
