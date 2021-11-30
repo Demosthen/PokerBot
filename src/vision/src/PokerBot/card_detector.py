8cc4cb0148e9ce13dd3eb8e6bc0ad6726b6ab904
Demosthen <1008981@gmail.com>
2021-11-30 14:41:53 -0800

    Use AlexeyAB darknet branch instead of pjreddie

--- src/vision/src/PokerBot/card_detector.py
+++ src/vision/src/PokerBot/card_detector.py
@@ -1,13 +1,15 @@
-from darknet import *
+from darknet.darknet_images import *
+from darknet import darknet
 from matplotlib import pyplot as plt
 
+#TODO: adapt image_classification function from darknet images to rework classify
 def classify(im, thresh=0.75):
-    net = load_net(b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/tiny2-coco.cfg", b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/tiny2_coco_100k_candidate.weights", 0)
+    net = load_net(b"tiny2-coco.cfg", b"tiny2_coco.weights", 0)
     # print("loading meta")
-    meta = load_meta(b"/home/cc/ee106a/fl21/class/ee106a-afr/ros_workspaces/pokerbot/src/vision/src/PokerBot/output.data")
+    meta = load_meta(b"output.data")
     #r = detect(net, meta, b"SDC13156.JPG", 0.75)
     
     r = detect_np(net, meta, im, thresh)
     return r
-im = plt.imread("test.png")
-print(classify(im, 0.5))
+im = plt.imread("e.jpg")
+print(classify(im, 0.7))
