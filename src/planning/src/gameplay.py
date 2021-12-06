4bfb6595b63afa3ff942ee90c2cd9b5e2c72cba1
Yuqing W <yuqingwang@berkeley.edu>
2021-12-06 09:48:49 -0800

    morning work, tentative gameplay class design

--- src/planning/src/gameplay.py
+++ src/planning/src/gameplay.py
@@ -1,15 +1,17 @@
 """
-Gameplay Class
+Gameplay Class: is this the one that controls the entire game??
 """
 
 from geometry_msgs.msg import Point
+from coord_client import two
+from planning.src.coord_client import Coord_Client
 
 class gameplay(object):
     """
     Gameplay class that keeps track of the game state,
     baxter's cards and returns a next move.
     """
-    def __init__(self, current_cards):
+    def __init__(self):
         """
         Constructor. 
         
@@ -17,30 +19,38 @@ class gameplay(object):
         current_cards: the list of Point objects and strings of all
         the cards currently on the table. The most recently
         played card index 0.
-        [["4D", Point], ["10H", Point], ...]
+        [["4D","10H",...], [Point1, Point2, ...]]
+        "h" "s" "c" "d"
         
         """
-        self.center_card = current_cards[0]
-        self.deck_of_cards = current_cards[1]
-        self.baxter_hand = current_cards[2:]
+        #Initiates coord_client and looks for cards
+        client = Coord_Client()
+        current_cards = []
+        current_cards = client.twodto3d()
+        # Scan for cards, if there's nothing, center card, baxter_hand = []
+        self.center_card = current_cards[0][0]
+        print("the center card is: " + self.center_card)
+        self.deck_of_cards = current_cards[1][1]
+        print("the deck of cards is located at: " + self.deck_of_cards)
+        self.baxter_hand = current_cards[0][:]
         self.turn = "baxter"
     
+
     # Compares the most recently played card to its own, and 
-    def compare_cards():
+    def compare_cards(self):
         target_card = []
         for card in self.baxter_hand:
-            # Card[0] accesses the coords, Card[1] is the string that contains the type
+            # Card[0] contains the card type, Card[1] accesses the coords
             # if either the card number or the suite is the same
-            ll = len(card[0])
-            if card[0].charAt(ll-1) == center_card[0].charAt(ll-1) or card[1][0:ll-2] == center_card[1][0:ll-2]:
+            
+            if card[-1] == self.center_card[-1] or card[0] == self.center_card[0] or card[1] == self.center_card[1]:
                 target_card = card
+                # baxter has a card that it can play, switch gamestate to player turn
+                self.turn = "player"
            
-        # if baxter has a card that it can play, switch gamestate to player turn
-        if target_card != null:
-            self.turn = "player"
-        else:
+        if target_card == []:
             #draw a new card from the deck
-            target_card = self.deck
+            target_card = self.deck_of_cards
 
         return target_card
 
